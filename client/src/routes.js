4092cc3575e52370f9df854349559b54be3cc3cd
Vickie <9098023+PhishyFish@users.noreply.github.com>
2017-12-12 10:01:01 -0800

    Fix routes file

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,12 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
-=======
-import NavBarContainer from './containers/NavBarContainer';
-import GameContainer from './containers/GameContainer';
-import LobbyContainer from './containers/LobbyContainer';
-import SessionContainer from './containers/SessionContainer';
-
->>>>>>> 5e9415dcd07e2fb550da0138c1edc5a968200572
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import NavBarContainer from './containers/NavBarContainer';
@@ -17,7 +9,6 @@ import GameContainer from './containers/GameContainer';
 
 const Routes = () => {
   return (
-<<<<<<< HEAD
     <div className="app-container">
       <Router>
         <div>
@@ -29,16 +20,6 @@ const Routes = () => {
         </div>
       </Router>
     </div>
-=======
-    <Router>
-      <div>
-        <Route path="/" component={NavBarContainer} />
-        <Route exact path="/signup" component={SessionContainer} />
-        <Route exact path="/lobby" component={LobbyContainer} />
-        <Route exact path="/game" component={GameContainer} />
-      </div>
-    </Router>
->>>>>>> 5e9415dcd07e2fb550da0138c1edc5a968200572
   );
 };
 
