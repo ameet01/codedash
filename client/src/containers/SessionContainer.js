f08584ad6c93a6b4a407656894a38b5f2218c57b
Vickie <9098023+PhishyFish@users.noreply.github.com>
2017-12-12 09:59:43 -0800

    Update files with master code

--- client/src/containers/SessionContainer.js
+++ client/src/containers/SessionContainer.js
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-import { connect } from 'react-redux';
-
-import Session from '../components/Session/Session';
-
-let mapStateToProps;
-let mapDispatchToProps;
-=======
 import * as Actions from '../actions';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
@@ -23,6 +15,5 @@ function mapDispatchToProps(dispatch) {
     actions: bindActionCreators(Actions, dispatch)
   };
 }
->>>>>>> 5e9415dcd07e2fb550da0138c1edc5a968200572
 
 export default connect(mapStateToProps, mapDispatchToProps)(Session);
