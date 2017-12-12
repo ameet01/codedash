b80b3a47c1bcc597e62ab25cf96663c23531d6f3
Vickie <9098023+PhishyFish@users.noreply.github.com>
2017-12-12 10:24:59 -0800

    Separate reset file

--- client/src/components/Splash/Splash.jsx
+++ client/src/components/Splash/Splash.jsx
@@ -7,7 +7,7 @@ class Splash extends React.Component {
 
   render() {
     return(
-      <div>
+      <div className="splash-container">
         
       </div>
     );
--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './routes';
 
+import './reset.css';
 import './index.css';
 
 import { Provider } from 'react-redux';
