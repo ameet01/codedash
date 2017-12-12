82deaf7983fce80ea9e9761f282e079d8236c0b6
Ameet Vadhia <ameet01@gmail.com>
2017-12-12 00:09:56 -0800

    Add errors to login

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 import * as actions from '../../actions';
 import classnames from 'classnames';
 import { Link } from 'react-router-dom';
-import logo from './logo.svg';
 import axios from 'axios';
 
 import './style.css';
@@ -11,7 +10,7 @@ import './style.css';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {username: "", password: ""};
+    this.state = {username: "", password: "", error: ""};
     this.handleSubmit = this.handleSubmit.bind(this);
     this.logout = this.logout.bind(this);
   }
@@ -27,11 +26,13 @@ class App extends Component {
 
   handleSubmit(e) {
     e.preventDefault();
-    axios.post('/login', {username: this.state.username, password: this.state.password}).then(() => this.props.fetchUser());
+    axios.post('/api/login', {username: this.state.username, password: this.state.password})
+    .then(() => this.props.fetchUser()).then(() => this.setState({error: ''}))
+    .catch(error => { this.setState({error: 'Invalid Credentials'}); });
   }
 
   logout() {
-    axios.get('./logout').then(() => this.props.fetchUser());
+    axios.get('./api/logout').then(() => this.props.fetchUser()).then(() => this.setState({username: "", password: "", error: ""}));
   }
 
   renderContent() {
@@ -40,8 +41,8 @@ class App extends Component {
         return;
       case false:
         return (<form method='post' onSubmit={(e) => this.handleSubmit(e)}>
-          <input type='text' onChange={this.update('username')} name='username' value={this.state.username} />
-          <input type='password'  onChange={this.update('password')} name='password' value={this.state.password} />
+          <input type='text' onChange={this.update('username')} name='username' value={this.state.username} autoComplete="new-username" />
+          <input type='password'  onChange={this.update('password')} name='password' value={this.state.password} autoComplete="new-password" />
           <button>Submit</button>
         </form>);
       default:
@@ -55,22 +56,10 @@ class App extends Component {
     return (
       <div className={classnames('App', className)} props={this.props}>
         {this.renderContent()}
-
+        {this.state.error}
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React - yay!</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Link to='about'><button>Test React Router</button></Link>
-        <br />
-        <br />
-        <button onClick={this.props.actions.expressTest}>Test if Express is working</button>
-        <br />
-        <br />
-        <button onClick={this.props.actions.dbTest}>Test if Express and Sequelize are working</button>
-        <div style={{ padding: '30px' }}>{this.props.results}</div>
       </div>
     );
   }
