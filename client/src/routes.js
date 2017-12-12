bbc773b5a4d8373ba7e59d599a7ba4cb11ab9b8c
Ameet Vadhia <ameet01@gmail.com>
2017-12-12 12:18:44 -0800

    Implement auth/protected routes

--- client/src/routes.js
+++ client/src/routes.js
@@ -8,16 +8,18 @@ import LobbyContainer from './containers/LobbyContainer';
 import GameContainer from './containers/GameContainer';
 import Footer from './components/Footer/Footer';
 
+import {AuthRoute, ProtectedRoute} from './util/route_util';
+
 const Routes = () => {
   return (
     <div className="app-container">
       <Router>
         <div>
           <Route path="/" component={NavBarContainer} />
-          <Route exact path="/" component={SplashContainer} />
-          <Route exact path="/signup" component={SessionContainer} />
-          <Route exact path="/lobby" component={LobbyContainer} />
-          <Route exact path="/game" component={GameContainer} />
+          <AuthRoute path="/" component={SplashContainer} />
+          <AuthRoute path="/signup" component={SessionContainer} />
+          <ProtectedRoute path="/lobby" component={LobbyContainer} />
+          <ProtectedRoute path="/game" component={GameContainer} />
           <Route path="/" component={Footer} />
         </div>
       </Router>
