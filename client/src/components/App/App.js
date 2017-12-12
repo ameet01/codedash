f6edb194be791087acb45c844a0427312b8658e1
Ameet Vadhia <ameet01@gmail.com>
2017-12-11 21:43:47 -0800

    changes

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -58,7 +58,7 @@ class App extends Component {
 
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React - Fullstack!</h2>
+          <h2>Welcome to React - yay!</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
