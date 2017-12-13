3f0eebe84a77f657fd2f3a708a7570d125251b13
Ameet Vadhia <ameet01@gmail.com>
2017-12-12 21:53:53 -0800

    Fix backspacing issue

--- client/src/components/Game/Game.jsx
+++ client/src/components/Game/Game.jsx
@@ -3,16 +3,36 @@ import { connect } from 'react-redux';
 import * as actions from '../../actions';
 import axios from 'axios';
 
-const code = `d3.interpolators = [
-  function(a, b) {
-    var t = typeof b;
-    return (t === "string" ? (d3_rgb_names.has(b) || /^(#|rgb\(|hsl\()/.test(b) ? d3_interpolateRgb : d3_interpolateString)
-        : b instanceof d3_Color ? d3_interpolateRgb
-        : t === "object" ? (Array.isArray(b) ? d3_interpolateArray : d3_interpolateObject)
-        : d3_interpolateNumber)(a, b);
-  }
-];
+const code = `render() {
+  return <div>
+    <h1>WHY GAME</h1>
+    <pre><code>{code.split('').map((char, index) => {
+        let span;
+        if(index === this.state.pointer) {
+          if(char === "/n") {
+            span = <span className='active enter' key={index}>{char}</span>;
+          } else {
+            if(this.state.incorrect) {
+              span = <span className='wrong' key={index}>{char}</span>;
+            } else {
+              span = <span className='active' key={index}>{char}</span>;
+            }
+          }
+        } else {
+          span = <span className='regular' key={index}>{char}</span>;
+        }
+
+        if(this.state.key === index) {
+          span = <span className='incorrect' key={index}>{char}</span>;
+        }
+        return span;
+      })}</code></pre>
+  </div>;
 `;
+
+const codeLength = code.split(/\s+/g).length - 1;
+
+
 
 class Game extends Component {
   constructor(props) {
@@ -32,10 +52,16 @@ class Game extends Component {
 
   registerKeyPress(e) {
     e.preventDefault();
+    console.log(this.state.pointer);
     if(this.state.wrongstreak <= 5) {
       if(e.keyCode === (code[this.state.pointer].charCodeAt(0)) && this.state.incorrect === false) {
         this.setState({pointer: this.state.pointer += 1, incorrect: false});
       } else if(code[this.state.pointer].charCodeAt(0) === 10 && e.keyCode === 13) {
+        if(this.state.incorrect === true) {
+        } else {
+          this.setState({incorrect: false});
+        }
+
         let num = this.state.pointer + 1;
         if(code[num]) {
           while(code[num].match(/\s/g)) {
@@ -43,7 +69,9 @@ class Game extends Component {
           }
           this.setState({pointer: num});
         }
-      } else {
+      } else if(code[this.state.pointer].charCodeAt(0) === 10 && e.keyCode !== 13) {
+        this.setState({incorrect: true});
+      }else {
         if(this.state.wrongstreak === 0) {
           this.setState({key: this.state.pointer}, this.setState({incorrect: true, pointer: this.state.pointer += 1, wrongstreak: this.state.wrongstreak += 1}));
         } else {
@@ -56,17 +84,37 @@ class Game extends Component {
   backspace(e) {
     if(this.state.incorrect === true && e.keyCode === 8) {
       if(this.state.wrongstreak > 1) {
-        this.setState({pointer: this.state.pointer -= 1, wrongstreak: this.state.wrongstreak -= 1 }, console.log(this.state.pointer));
+        let num = this.state.pointer - 1;
+        let original = num;
+        if(code[num]) {
+          let string = "";
+          while(code[num].match(/\s/g)) {
+            string+=code[num];
+            num -= 1;
+          }
+          if(string.includes("\n")) {
+            this.setState({pointer: num, wrongstreak: this.state.wrongstreak -= 1 }, console.log(this.state.pointer));
+          } else {
+            this.setState({pointer: original, wrongstreak: this.state.wrongstreak -= 1});
+          }
+        }
       } else {
         this.setState({pointer: this.state.pointer -= 1, incorrect: false, key: undefined, wrongstreak: 0}, console.log(this.state.pointer));
       }
     } else if(this.state.incorrect === false && e.keyCode === 8) {
       let num = this.state.pointer - 1;
+      let original = num;
       if(code[num]) {
+        let string = "";
         while(code[num].match(/\s/g)) {
+          string+=code[num];
           num -= 1;
         }
-        this.setState({pointer: num});
+        if(string.includes("\n")) {
+          this.setState({pointer: num});
+        } else {
+          this.setState({pointer: original});
+        }
       }
       // this.setState({pointer: this.state.pointer -= 1});
     }
