cee34090caef8b61cddee3c2c5483b89abc1e1e7
Ameet Vadhia <ameet01@gmail.com>
2017-12-12 19:45:25 -0800

    Major progress to funtionality of typing test

--- client/src/components/Game/Game.jsx
+++ client/src/components/Game/Game.jsx
@@ -3,54 +3,102 @@ import { connect } from 'react-redux';
 import * as actions from '../../actions';
 import axios from 'axios';
 
-let code = `class Game extends Component {
-  constructor(props) {
-    super(props);
-  }
+let code = `d3.interpolate = d3_interpolate;
 
-  render() {
-    return <div></div>;
-  }
+function d3_interpolate(a, b) {
+  var i = d3.interpolators.length, f;
+  while (--i >= 0 && !(f = d3.interpolators[i](a, b)));
+  return f;
 }
 
-export default connect(null, actions)(Game);
+d3.interpolators = [
+  function(a, b) {
+    var t = typeof b;
+    return (t === "string" ? (d3_rgb_names.has(b) || /^(#|rgb\(|hsl\()/.test(b) ? d3_interpolateRgb : d3_interpolateString)
+        : b instanceof d3_Color ? d3_interpolateRgb
+        : t === "object" ? (Array.isArray(b) ? d3_interpolateArray : d3_interpolateObject)
+        : d3_interpolateNumber)(a, b);
+  }
+];
 `;
 
 class Game extends Component {
   constructor(props) {
     super(props);
-    this.state = {pointer: 0};
+    this.state = {pointer: 0, incorrect: false, wrongstreak: 0, key: undefined};
   }
 
   componentDidMount() {
     document.addEventListener('keypress', (e) => this.registerKeyPress(e));
+    document.addEventListener('keydown', (e) => this.backspace(e));
   }
 
   componentWillUnmount() {
     document.removeEventListener('keypress', (e) => this.registerKeyPress(e));
+    document.removeEventListener('keydown', (e) => this.backspace(e));
   }
 
   registerKeyPress(e) {
     e.preventDefault();
-    if(e.keyCode === (code[this.state.pointer].charCodeAt(0))) {
-      this.setState({pointer: this.state.pointer += 1});
-    } else if(code[this.state.pointer].charCodeAt(0) === 10 && e.keyCode === 13) {
-      this.setState({pointer: this.state.pointer += 1});
+    if(this.state.wrongstreak <= 5) {
+      if(e.keyCode === (code[this.state.pointer].charCodeAt(0))) {
+        this.setState({pointer: this.state.pointer += 1, incorrect: false});
+      } else if(code[this.state.pointer].charCodeAt(0) === 10 && e.keyCode === 13) {
+        let num = this.state.pointer + 1;
+        if(code[num]) {
+          while(code[num].match(/\s/g)) {
+            num += 1;
+          }
+          this.setState({pointer: num});
+        }
+      } else {
+        if(this.state.wrongstreak === 0) {
+          this.setState({key: this.state.pointer}, this.setState({incorrect: true, pointer: this.state.pointer += 1, wrongstreak: this.state.wrongstreak += 1}));
+        } else {
+          this.setState({incorrect: true, pointer: this.state.pointer += 1, wrongstreak: this.state.wrongstreak += 1});
+        }
+      }
+    }
+
+
+  }
+
+  backspace(e) {
+    if(this.state.incorrect === true && e.keyCode === 8) {
+      if(this.state.wrongstreak > 1) {
+        this.setState({pointer: this.state.pointer -= 1, wrongstreak: this.state.wrongstreak -= 1 }, console.log(this.state.pointer));
+      } else {
+        this.setState({pointer: this.state.pointer -= 1, incorrect: false, key: undefined, wrongstreak: 0}, console.log(this.state.pointer));
+      }
+    } else if(this.state.incorrect === false && e.keyCode === 8) {
+      this.setState({pointer: this.state.pointer -= 1});
     }
   }
 
   render() {
     return <div>
       <h1>WHY GAME</h1>
-      <pre>{code.split('').map((char, index) => {
+      <pre><code>{code.split('').map((char, index) => {
           let span;
           if(index === this.state.pointer) {
-            span = <span className='active'>{char}</span>;
+            if(char === "\n") {
+              span = <span className='active enter' key={index}>{char}</span>;
+            } else {
+              if(this.state.incorrect) {
+                span = <span className='wrong' key={index}>{char}</span>;
+              } else {
+                span = <span className='active' key={index}>{char}</span>;
+              }
+            }
           } else {
-            span = <span className='regular'>{char}</span>;
+            span = <span className='regular' key={index}>{char}</span>;
+          }
+
+          if(this.state.key === index) {
+            span = <span className='incorrect' key={index}>{char}</span>;
           }
           return span;
-        })}</pre>
+        })}</code></pre>
     </div>;
 
   }
