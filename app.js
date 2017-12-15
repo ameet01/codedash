532c34f3dab4904620fa3f4ee014b8b5fa9a41c2
Ameet Vadhia <ameet01@gmail.com>
2017-12-15 12:56:45 -0800

    server stuff

--- app.js
+++ app.js
@@ -43,7 +43,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+} else {
+  app.use(express.static(path.join(__dirname, 'public')));
+}
+
 
 app.use('/', index);
 app.use('/users', users);
