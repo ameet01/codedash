ad47328e47b48fa0c64151f08ba161fa1e399a0a
Ameet Vadhia <ameet01@gmail.com>
2017-12-15 12:17:39 -0800

    Merge

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './routes';
 
-
 import './styles/reset.css';
 import './styles/index.css';
 import './styles/navbar.css';
