b0e44592b78bb8825f32d4dcb552cecf18c38247
Ameet Vadhia <ameet01@gmail.com>
2017-12-15 17:17:42 -0800

    trying slash

--- app.js
+++ app.js
@@ -54,20 +54,11 @@ if (process.env.NODE_ENV === 'production') {
 
   // Express will serve up the index.html file
   // if it doesn't recognize the route
-  app.get('*', (req, res) => {
+  app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
-app.use(function(req, res, next) {
- res.setHeader('Access-Control-Allow-Origin', '*');
- res.setHeader('Access-Control-Allow-Credentials', 'true');
- res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
- res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
-//and remove cacheing so we get the most recent appointments
- res.setHeader('Cache-Control', 'no-cache');
- next();
-});
 
 app.use('/', index);
 app.use('/users', users);
